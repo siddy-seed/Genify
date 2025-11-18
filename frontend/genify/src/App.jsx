f6aae9ca2bef0cd904e9099638fe0c9763c65d48
Siddhant Giri <siddyseed@Siddhants-MacBook-Pro-2.local>
2025-11-18 13:06:06 +0530

    resolve routes

--- frontend/genify/src/App.jsx
+++ frontend/genify/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
 import About from "./about";
 import SignIn from "./signin";
 function App() {
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Navigate to="/signIn" replace />} />
         <Route path="/signIn" element={<SignIn />} />
       </Routes>
     </BrowserRouter>
